f6c8340fcfbb4c5969732f81d50e60c0949567b2
Evgeny1304 <erhabirov@gmail.com>
2020-06-04 20:24:04 +0300

    Добавлена генерация меток на страницу.

---
+++ js/main.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var OFFER_TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var OFFER_CHECKIN = [12, 13, 14];
+var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var MAX_OFFER_COUNT = 8;
+var MAP_PIN_WIDTH = 50;
+var MAP_PIN_HEIGHT = 70;
+
+var getRandom = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+var getRandomArray = function (arr) {
+  var randomArr = [];
+  var randomIndex = getRandom(0, arr.length - 1);
+
+  for (var i = randomIndex; i < arr.length; i++) {
+    randomArr.push(arr[i]);
+  }
+
+  return randomArr;
+};
+
+var getOffers = function () {
+  var offers = [];
+
+  for (var i = 0; i < MAX_OFFER_COUNT; i++) {
+    var locationX = getRandom(0, 1200);
+    var locationY = getRandom(130, 630);
+
+    var offer = {
+      'author': {
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      'offer': {
+        'title': 'Сдается в аренду',
+        'address': locationX + ', ' + locationY,
+        'price': getRandom(0, 1000000),
+        'type': OFFER_TYPE[getRandom(0, OFFER_TYPE.length - 1)],
+        'rooms': getRandom(1, 100),
+        'guests': getRandom(0, 3),
+        'checkin': OFFER_CHECKIN[getRandom(0, OFFER_CHECKIN.length - 1)].toString() + ':00',
+        'checkout': OFFER_CHECKIN[getRandom(0, OFFER_CHECKIN.length - 1)].toString() + ':00',
+        'features': getRandomArray(OFFER_FEATURES),
+        'description': 'Описание',
+        'photos': getRandomArray(OFFER_PHOTOS)
+      },
+      'location': {
+        'x': locationX,
+        'y': locationY
+      },
+    };
+    offers.push(offer);
+  }
+
+  return offers;
+};
+
+var offers = getOffers();
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+var renderMapPin = function (pin) {
+  var mapPin = mapPinTemplate.cloneNode(true);
+
+  mapPin.style.left = (pin.location.x + MAP_PIN_WIDTH / 2) + 'px';
+  mapPin.style.top = (pin.location.y - MAP_PIN_HEIGHT) + 'px';
+  mapPin.querySelector('img').src = pin.author.avatar;
+  mapPin.querySelector('img').alt = pin.offer.title;
+
+  return mapPin;
+};
+
+var mapPins = document.querySelector('.map__pins');
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < offers.length; i++) {
+  fragment.appendChild(renderMapPin(offers[i]));
+}
+mapPins.appendChild(fragment);
